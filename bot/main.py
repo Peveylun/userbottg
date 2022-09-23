fda4270b1024d8788ce6c7adefc5d2eb9da14b22
Peveylun <cobzikl@gmail.com>
2022-09-24 00:13:37 +0300

    remove function, idk

--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,8 @@
-from pyrogram import Client, filters
-from pyrogram.raw.types import Message
+from pyrogram import Client
 
 from misc.env import TgKeys
 
 app = Client('my_account', api_id=TgKeys.API_ID, api_hash=TgKeys.API_HASH, plugins=dict(root="plugins"))
-
-
-@app.on_message(filters.command('gch', prefixes='.') & filters.me)
-async def get_chat(_, msg: Message):
-    users_id = []
-    async for i in app.get_chat_members(msg.chat.id):
-        users_id.append(i.user.id)
-    print(await app.get_users(users_id))
 
 
 def start_bot() -> None:
