b1705c7bfeae922c794a6cdca432c8e2b52a866e
Peveylun <cobzikl@gmail.com>
2024-09-17 16:47:21 +0300

    added some stuff

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,11 @@ from pyrogram import Client
 
 from misc.env import TgKeys
 
-app = Client('my_account', api_id=TgKeys.API_ID, api_hash=TgKeys.API_HASH, plugins=dict(root="plugins"))
+plugins = dict(root="plugins",
+               include=["fun.some_fun heart who type",
+                        "todo.db add get delete"])
+
+app = Client('my_account', api_id=TgKeys.API_ID, api_hash=TgKeys.API_HASH, plugins=plugins)
 
 
 def start_bot() -> None:
